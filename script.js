70bb8c6d871c6d0b38094ad18da0386d82e343f4
93913162+yasinpathan77@users.noreply.github.com <93913162+yasinpathan77@users.noreply.github.com>
2023-09-02 12:23:31 +0530

    Created Functions for basic Math.

---
+++ script.js
@@ -0,0 +1,16 @@
+function Add(a, b) {
+    return a + b;
+}
+
+function Subtract(a, b) {
+    return a - b;
+}
+
+function Multiply(a, b) {
+    return a * b;
+}
+
+function Divide(a, b) {
+    if (b == 0) return "Can't Divide with zero";
+    return a / b;
+}
