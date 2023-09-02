7446d6f5ee3964c246f18460f084bfe0839697d3
93913162+yasinpathan77@users.noreply.github.com <93913162+yasinpathan77@users.noreply.github.com>
2023-09-02 12:29:34 +0530

    Created Operate Function to handle calculation.

--- script.js
+++ script.js
@@ -17,3 +17,11 @@ function Divide(a, b) {
     if (b == 0) return "Can't Divide with zero";
     return a / b;
 }
+
+function Operate(fNumber, sNumber, operat) {
+    if (operat === "+") return Add(fNumber, sNumber);
+    else if (operat === "-") return Subtract(fNumber, sNumber);
+    else if (operat === "*") return Multiply(fNumber, sNumber);
+    else if (operat === "/") return Divide(fNumber, sNumber);
+    else return "Error";
+}
