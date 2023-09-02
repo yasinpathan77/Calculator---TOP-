3a0e898298a5cb31408ea5b25695fd9cde843755
93913162+yasinpathan77@users.noreply.github.com <93913162+yasinpathan77@users.noreply.github.com>
2023-09-02 18:59:26 +0530

    Create Function to display number when clicked

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let firstNumber = 0, secondNumber = 0, operator = "+";
-
+let displayNumber = 0;
 
 function Add(a, b) {
     return a + b;
@@ -25,3 +25,30 @@ function Operate(fNumber, sNumber, operat) {
     else if (operat === "/") return Divide(fNumber, sNumber);
     else return "Error";
 }
+
+function Populate() {
+    document.querySelectorAll(".number").forEach((element) => {
+        element.addEventListener("click", (data) => {
+            let value = element.getAttribute("data-number");
+            console.log(value);
+            SetDisplayNumber(value)
+        });
+    });
+}
+
+function SetDisplayNumber(num) {
+    var display = document.querySelector(".display");
+    if (displayNumber === 0) {
+        if (num === '0') return;
+        displayNumber = num;
+    }
+    else {
+        if (displayNumber.toLocaleString().includes('.') && num == '.') return;
+        displayNumber += num;
+    }
+    display.innerText = displayNumber;
+}
+
+
+window.onload = Populate();
+
